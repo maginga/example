50a007a15dc17f66ad16e487efa961771a6a8e13
hansonjang <jdjang@bistel.com>
2020-09-15 13:32:47 +0900

    new activity

---
+++ activity/binary2json/activity.go
@@ -0,0 +1,139 @@
+package binary2json
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/project-flogo/core/activity"
+	"github.com/project-flogo/core/data/metadata"
+)
+
+// Activity define activity object
+type Activity struct {
+	settings *Settings
+}
+
+type Header struct {
+	totalSize uint32
+	dataCount uint32
+	datetime  string
+	null      byte
+}
+
+type Record struct {
+	data []float32
+	cr   byte
+	lf   byte
+}
+
+func init() {
+	_ = activity.Register(&Activity{}, New)
+}
+
+var activityMd = activity.ToMetadata(&Input{}, &Output{})
+
+// New create a new kafka activity
+func New(ctx activity.InitContext) (activity.Activity, error) {
+	s := &Settings{}
+	err := metadata.MapToStruct(ctx.Settings(), s, true)
+	if err != nil {
+		return nil, err
+	}
+
+	act := &Activity{settings: s}
+	return act, nil
+}
+
+// Metadata returns the activity's metadata
+func (a *Activity) Metadata() *activity.Metadata {
+	return activityMd
+}
+
+// Eval evaluate
+func (a *Activity) Eval(context activity.Context) (done bool, err error) {
+	logger := context.Logger()
+	logger.Info("Executing BINARY2JSON activity")
+
+	input := &Input{}
+	err = context.GetInputObject(input)
+	if err != nil {
+		return false, err
+	}
+	fileName := input.FileName
+	file, err := os.Open(fileName)
+	if err != nil {
+
+	}
+	defer file.Close()
+
+	columns := a.settings.Columns
+
+	headerSize := 32 // bytes
+	// rows := []string{}
+	for {
+		header := Header{}
+		headerBytes := readNextBytes(file, headerSize)
+
+		buffer1 := bytes.NewBuffer(headerBytes)
+		err = binary.Read(buffer1, binary.LittleEndian, &header)
+		if err != nil {
+			log.Fatal("binary.Read failed", err)
+		}
+
+		record := Record{}
+		dataSize := int(header.totalSize) - headerSize
+		dataBytes := readNextBytes(file, dataSize)
+
+		buffer2 := bytes.NewBuffer(dataBytes)
+		err = binary.Read(buffer2, binary.LittleEndian, &record)
+		if err != nil {
+			log.Fatal("binary.Read failed", err)
+		}
+
+		// var json string
+		// values := []string{}
+
+		for i := 1; i <= len(columns); i++ {
+			logger.Info(fmt.Sprintf("%v", columns[i]) + ": " + fmt.Sprintf("%v", record.data[i]))
+		}
+
+		// for real := range record.data {
+		// 	logger.Info(real)
+		// }
+		// json = "{" + strings.Join(values, ",") + "}"
+		// rows = append(rows, json)
+	}
+
+	// logger.Info("rows: ", len(rows))
+	// output := &Output{Message: rows}
+	// err = context.SetOutputObject(output)
+	// if err != nil {
+	// 	return false, err
+	// }
+
+	logger.Info("BINARY2JSON activity completed")
+	return true, nil
+}
+
+func readNextBytes(file *os.File, number int) []byte {
+	bytes := make([]byte, number)
+
+	_, err := file.Read(bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return bytes
+}
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
---
+++ activity/binary2json/metadata.go
@@ -0,0 +1,51 @@
+package binary2json
+
+import (
+	"github.com/project-flogo/core/data/coerce"
+)
+
+// Settings setting struct
+type Settings struct {
+	Columns map[int]interface{} `md:"columns"` // data columns
+}
+
+// Input input
+type Input struct {
+	FileName string `md:"fileName"` //
+}
+
+func (i *Input) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"fileName": i.FileName,
+	}
+}
+
+func (i *Input) FromMap(values map[string]interface{}) error {
+
+	var err error
+	i.FileName, err = coerce.ToString(values["fileName"])
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type Output struct {
+	Message interface{} `md:"message"` // The data that sent kafka
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"message": o.Message,
+	}
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+	var err error
+	o.Message, err = coerce.ToObject(values["message"])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
