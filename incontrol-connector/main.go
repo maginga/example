582bba0ce4f7dac4dd3a5756762b3cfcd2fa6a4d
hanson jang <hansonjang@gmail.com>
2021-10-01 09:57:07 +0900

    fixed

--- incontrol-connector/main.go
+++ incontrol-connector/main.go
@@ -14,6 +14,22 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+type SensorData struct {
+	Data []DataValue
+}
+
+type DataValue struct {
+	Ai    []float32
+	Aiabh []bool
+	Aiabl []bool
+	Aialh []bool
+	Aiall []bool
+	Di    []bool
+	Diab  []bool
+	Dial  []bool
+	Time  string
+}
+
 func main() {
 	filename, _ := filepath.Abs("./config.yaml")
 	yamlFile, err := ioutil.ReadFile(filename)
@@ -42,63 +58,66 @@ func main() {
 		schedTime = 1
 	}
 
-	// tr := &http.Transport{
-	// 	MaxIdleConns:       10,
-	// 	IdleConnTimeout:    30 * time.Second,
-	// 	DisableCompression: true,
-	// }
-	// client := &http.Client{Transport: tr}
-
 	s := gocron.NewScheduler(time.UTC)
 	s.Every(schedTime).Seconds().Do(func() {
 		// data can be acquired
 		point := strconv.Itoa(config.Points)
-		sensorData := Call(config.RestUrl + point)
+		bodyBytes, err := GetBody(config.RestUrl + point)
+		if err != nil {
+			log.Println("unable to retrieve the response body from the Purewafer API server: %v", err)
+		} else {
+			var sensorData SensorData
+			json.Unmarshal(bodyBytes, &sensorData)
 
-		for i := range sensorData.Data {
-			value := sensorData.Data[len(sensorData.Data)-1-i]
+			if sensorData.Data != nil {
+				for i := range sensorData.Data {
+					value := sensorData.Data[len(sensorData.Data)-1-i]
 
-			timezone, _ := time.LoadLocation(config.LocalTimeZone)
-			sensorTime, err := ParseIn(value.Time, timezone)
-			if err != nil {
-				log.Printf("err: %v", err)
-			}
+					timezone, _ := time.LoadLocation(config.LocalTimeZone)
+					sensorTime, err := ParseIn(value.Time, timezone)
+					if err != nil {
+						log.Printf("err: %v", err)
+					}
 
-			if lastTime.Before(sensorTime) {
-				valueMap := make(map[string]interface{})
-				valueMap["assetId"] = config.AssetName
-				valueMap["sensorType"] = config.SensorType
-				valueMap["sensorId"] = config.SensorId
-				valueMap["event_time"] = sensorTime.UTC().Format(time.RFC3339)
+					if lastTime.Before(sensorTime) {
+						valueMap := make(map[string]interface{})
+						valueMap["assetId"] = config.AssetName
+						valueMap["sensorType"] = config.SensorType
+						valueMap["sensorId"] = config.SensorId
+						valueMap["event_time"] = sensorTime.UTC().Format(time.RFC3339)
 
-				for i, val := range value.Ai {
-					paramName := "analog_input_" + strconv.Itoa(i+1)
-					valueMap[paramName] = val
+						for i, val := range value.Ai {
+							paramName := "analog_input_" + strconv.Itoa(i+1)
+							valueMap[paramName] = val
+						}
+
+						msgBytes, _ := json.Marshal(valueMap)
+						message := string(msgBytes)
+						msg := &sarama.ProducerMessage{
+							Topic: config.Topic,
+							Value: sarama.StringEncoder(message),
+						}
+
+						log.Printf("[%s] [%d] msg: %s\n", config.AssetName, i, message)
+
+						partition, offset, err := producer.SendMessage(msg)
+						if err != nil {
+							log.Printf("[%s] > FAILED to send message: %s\n", config.AssetName, err)
+						} else {
+							log.Printf("[%s] > message sent to partition %d at offset %d\n", config.AssetName, partition, offset)
+						}
+
+						// for kafka
+						time.Sleep(time.Millisecond * 200)
+					} else {
+						log.Print("This message has already been delivered.")
+					}
+
+					lastTime = sensorTime
 				}
-
-				msgBytes, _ := json.Marshal(valueMap)
-				message := string(msgBytes)
-				msg := &sarama.ProducerMessage{
-					Topic: config.Topic,
-					Value: sarama.StringEncoder(message),
-				}
-
-				log.Printf("[%s] [%d] msg: %s\n", config.AssetName, i, message)
-
-				partition, offset, err := producer.SendMessage(msg)
-				if err != nil {
-					log.Printf("[%s] > FAILED to send message: %s\n", config.AssetName, err)
-				} else {
-					log.Printf("[%s] > message sent to partition %d at offset %d\n", config.AssetName, partition, offset)
-				}
-
-				// for kafka
-				time.Sleep(time.Millisecond * 200)
 			} else {
-				log.Print("This message has already been delivered.")
+				log.Print("There's no response.")
 			}
-
-			lastTime = sensorTime
 		}
 	})
 	// starts the scheduler and blocks current execution path
--- incontrol-connector/restapi.go
+++ incontrol-connector/restapi.go
@@ -1,48 +1,34 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
-type SensorData struct {
-	Data []DataValue
-}
-
-type DataValue struct {
-	Ai    []float32
-	Aiabh []bool
-	Aiabl []bool
-	Aialh []bool
-	Aiall []bool
-	Di    []bool
-	Diab  []bool
-	Dial  []bool
-	Time  string
-}
-
-func Call(url string) SensorData {
+func GetBody(url string) ([]byte, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Print(err.Error())
+		return nil, err
 	}
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
+
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Print(err.Error())
+		return nil, err
 	}
 	defer resp.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Print(err.Error())
-	}
 
-	var sensorDataObj SensorData
-	json.Unmarshal(bodyBytes, &sensorDataObj)
-	//log.Printf("API Response as struct %+v\n", sensorDataObj)
-	return sensorDataObj
+	var bodyBytes []byte
+	if resp.StatusCode == 200 {
+		bodyBytes, err = ioutil.ReadAll(resp.Body)
+	} else if err != nil {
+		return nil, err
+	} else {
+		return nil, fmt.Errorf("The remote end did not return a HTTP 200 (OK) response.")
+	} 
+
+	return bodyBytes, nil
 }
